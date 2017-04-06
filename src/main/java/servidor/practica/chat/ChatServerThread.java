a985d82fc2d01f3ce93f335e6fc7c2a1768b7470
santipecin <santi_pecin@hotmail.com>
2017-04-06 15:44:56 -0300

    feli indenta desgraciado

--- src/main/java/servidor/practica/chat/ChatServerThread.java
+++ src/main/java/servidor/practica/chat/ChatServerThread.java
@@ -1,46 +1,58 @@
 package servidor.practica.chat;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class ChatServerThread extends Thread
-{  private Socket          socket   = null;
-   private Servidor      server   = null;
-   private int             ID       = -1;
-   private DataInputStream streamIn =  null;
-   private DataOutputStream streamOut = null;
+{  
+	private Socket socket = null;
+	private Servidor server = null;
+	private int ID = -1;
+	private DataInputStream streamIn =  null;
+	private DataOutputStream streamOut = null;
    
    public ChatServerThread(Servidor _server, Socket _socket)
-   {  server = _server;  socket = _socket;  ID = socket.getPort();
+   {  
+	   server = _server;
+	   socket = _socket;
+	   ID = socket.getPort();
    }
    
    public void run()
-   {  System.out.println("Server Thread " + ID + " running.");
-   	  System.out.println("El id del que se conecto es: ");
-   	  try
-   	  { 
-   		  String id = streamIn.readUTF();
-   		  System.out.println(id);
-   		  Usuario usuario = new Usuario(socket, id);
-   		  server.addUsuario(usuario);
-   		  while (true)
-   		  {   
-    	  //streamOut.writeUTF("merlusa");
-   			  streamIn.readUTF(); //Queda  a la espera de nuevos mensajes este.
-   		  }
+   {  
+	   System.out.println("Server Thread " + ID + " running.");
+	   System.out.println("El id del que se conecto es: ");
+	   try
+   	  {
+		   String id = streamIn.readUTF();
+		   System.out.println(id);
+		   Usuario usuario = new Usuario(socket, id);
+		   server.addUsuario(usuario);
+		   while (true)
+		   {
+			   streamIn.readUTF(); //Queda  a la espera de nuevos mensajes este.
+		   }
          
-      }catch(IOException ioe) {  }
+   	  }catch(IOException ioe) {  }
    }
+   
    public void open() throws IOException
-   {  streamIn = new DataInputStream(socket.getInputStream());
-   	  streamOut = new DataOutputStream(socket.getOutputStream());
+   {  
+	   streamIn = new DataInputStream(socket.getInputStream());
+	   streamOut = new DataOutputStream(socket.getOutputStream());
    }
+   
    public void close() throws IOException
-   {  if (socket != null)    socket.close();
-      if (streamIn != null)  streamIn.close();
+   {  
+	   if (socket != null)
+	   {
+		   socket.close();
+	   }
+	   if (streamIn != null)
+	   {
+		   streamIn.close();
+	   }
    }
 }
--- src/main/java/servidor/practica/chat/Usuario.java
+++ src/main/java/servidor/practica/chat/Usuario.java
@@ -2,15 +2,19 @@ package servidor.practica.chat;
 
 import java.net.Socket;
 
-public class Usuario {
+public class Usuario 
+{
 	String id;
 	Socket socket;
 	
-	public Usuario(Socket socket, String id){
+	public Usuario(Socket socket, String id)
+	{
 		this.socket = socket;
 		this.id = id;
 	}
-	public Boolean soyUsuario(String id_){
+	
+	public Boolean soyUsuario(String id_)
+	{
 		return id.equals(id_);
 	}
 }
