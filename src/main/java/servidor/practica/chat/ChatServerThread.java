592235dd3bc7fd7755fac032842d160f89b6f86c
santipecin <santi_pecin@hotmail.com>
2017-04-06 17:20:38 -0300

    cambio bldecse

--- src/main/java/servidor/practica/chat/ChatServerThread.java
+++ src/main/java/servidor/practica/chat/ChatServerThread.java
@@ -6,53 +6,55 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ChatServerThread extends Thread
-{  
+{
 	private Socket socket = null;
 	private Servidor server = null;
 	private int ID = -1;
 	private DataInputStream streamIn =  null;
 	private DataOutputStream streamOut = null;
-   
-   public ChatServerThread(Servidor _server, Socket _socket)
-   {  
-	   server = _server;
-	   socket = _socket;
-	   ID = socket.getPort();
-   }
-   
-   public void run()
-   {  
-	   System.out.println("Server Thread " + ID + " running.");
-	   System.out.println("El id del que se conecto es: ");
-	   try
-   	  {
-		   String id = streamIn.readUTF();
-		   System.out.println(id);
-		   Usuario usuario = new Usuario(socket, id);
-		   server.addUsuario(usuario);
-		   while (true)
-		   {
-			   streamIn.readUTF(); //Queda  a la espera de nuevos mensajes este.
-		   }
-         
-   	  }catch(IOException ioe) {  }
-   }
-   
-   public void open() throws IOException
-   {  
-	   streamIn = new DataInputStream(socket.getInputStream());
-	   streamOut = new DataOutputStream(socket.getOutputStream());
-   }
-   
-   public void close() throws IOException
-   {  
-	   if (socket != null)
-	   {
-		   socket.close();
-	   }
-	   if (streamIn != null)
-	   {
-		   streamIn.close();
-	   }
-   }
+	
+	public ChatServerThread(Servidor _server, Socket _socket)
+	{
+		server = _server;
+		socket = _socket;
+		ID = socket.getPort();
+	}
+
+	public void run()
+	{
+		System.out.println("Server Thread " + ID + " running.");
+		try
+		{
+			String id = streamIn.readUTF();
+			System.out.println(id);
+			Usuario usuario = new Usuario(socket, id);
+			server.addUsuario(usuario);
+			while (true)
+			{
+				streamIn.readUTF(); //Queda  a la espera de nuevos mensajes.
+			}
+		}
+		catch(IOException ioe) 
+		{
+			System.out.println("Error : " + ioe);
+		}
+	}
+	
+	public void open() throws IOException
+	{
+		streamIn = new DataInputStream(socket.getInputStream());
+		streamOut = new DataOutputStream(socket.getOutputStream());
+	}
+	
+	public void close() throws IOException
+	{
+		if (socket != null)
+		{
+			socket.close();
+		}
+		if (streamIn != null)
+		{
+			streamIn.close();
+		}
+	}
 }
--- src/main/java/servidor/practica/chat/Servidor.java
+++ src/main/java/servidor/practica/chat/Servidor.java
@@ -5,25 +5,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Servidor implements Runnable
 {
-	
 	public List<Usuario> usuariosConectados;
 	private ServerSocket socketS;
 	private Socket socketC;
 	private Thread thread;
-	private ChatServerThread peticionCliente;
+	private List<ChatServerThread> chats;
 	
 	public Servidor(int puerto)
 	{
 		usuariosConectados = new ArrayList<Usuario>();
-		try {
+		try 
+		{
 			socketS = new ServerSocket(puerto);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		catch (IOException e) 
+		{
+			System.out.println("Error " + e);
 		}
 		System.out.println("Arranca server..");
 		this.start();
@@ -33,19 +33,24 @@ public class Servidor implements Runnable
 	public void run()
 	{
 		while (thread != null)
-		{  try
-	         {  System.out.println("Waiting for a client ..."); 
+		{  
+			try
+	        {  
+				System.out.println("Waiting for a client ..."); 
 	            addThread(socketS.accept());
-	         }
-	         catch(IOException ie)
-	         {  System.out.println("Acceptance Error: " + ie); }
-	    }		
+	        }
+	        catch(IOException ie)
+	        {  
+	        	System.out.println("Acceptance Error: " + ie);
+	        }
+	    }
 	}
 	
 	public void addThread(Socket socket)
 	{
 		System.out.println("Client accepted: " + socket);
-	    peticionCliente = new ChatServerThread(this, socket);
+		ChatServerThread peticionCliente = new ChatServerThread(this, socket);
+		chats.add(peticionCliente);
 	    try
 	    {  
 	    	peticionCliente.open();
@@ -62,15 +67,15 @@ public class Servidor implements Runnable
 		this.usuariosConectados.add(usuario);
 	}
 	
-	public Usuario darUsuario(String id)
+	public Usuario getUsuario(String id)
 	{
 		return this.usuariosConectados.stream().filter(usr->usr.soyUsuario(id)).findFirst().orElseGet(null);
 	}
 	
 	public void start()
-	{  
+	{
 		if (thread == null)
-	    {  
+	    {
 			thread = new Thread(this); 
 	        thread.start();
 	    }
@@ -80,7 +85,7 @@ public class Servidor implements Runnable
 	{
 		if (thread != null)
 	    {
-			thread.stop(); 
+			thread.stop();
 	        thread = null;
 	    }
 	}
