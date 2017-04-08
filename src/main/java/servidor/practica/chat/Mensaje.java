b166dffd4fb73fbea40634baf0dc8ca1f42534eb
santipecin <santi_pecin@hotmail.com>
2017-04-08 18:37:29 -0300

    agrego mensaje de cierre

--- src/main/java/servidor/practica/chat/Mensaje.java
+++ src/main/java/servidor/practica/chat/Mensaje.java
@@ -11,6 +11,11 @@ public class Mensaje {
 		this.mensaje = mensaje;
 	}
 	
+	public static String terminar_conexion()
+	{
+		return "end_of_conection";
+	}
+	
 	public void intentarEnviar(Pendientes pendientes, Servidor server){
 		Usuario usuarioReceptor = server.getUsuario(receptor);
 		if(usuarioReceptor == null){
